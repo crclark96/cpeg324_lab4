01e2f73c727476a1857d1bd43fb9544f25c2da4a
Collin Clark <crclark96@gmail.com>
2018-05-09 16:32:28 -0400

    mod controller to have intermediate registers
    (Zach we still need to add some nop tests but IDGAF right now)

--- controller.vhdl
+++ controller.vhdl
@@ -6,7 +6,7 @@
 -- Author     : Collin Clark  <collinclark@Collins-MacBook-Pro.local>
 -- Company    : 
 -- Created    : 2018-04-22
--- Last update: 2018-04-28
+-- Last update: 2018-05-09
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -28,13 +28,15 @@ entity controller is
 
   port (
     I             : in  std_logic_vector(7 downto 0);
-    enable_write  : out std_logic;
-    mux_in        : out std_logic;
-    add_sub       : out std_logic;
-    enable_output : out std_logic;
-    load          : out std_logic;
-    offset        : out std_logic;
-    compare       : out std_logic
+    clk           : in  std_logic;
+    enable_write  : out std_logic_vector(2 downto 0);
+    mux_in        : out std_logic_vector(2 downto 0);
+    add_sub       : out std_logic_vector(2 downto 0);
+    enable_output : out std_logic_vector(2 downto 0);
+    load          : out std_logic_vector(2 downto 0);
+    offset        : out std_logic_vector(2 downto 0);
+    compare       : out std_logic_vector(2 downto 0);
+    nop           : out std_logic_vector(2 downto 0)
     );
 
 end entity controller;
@@ -42,18 +44,67 @@ end entity controller;
 -------------------------------------------------------------------------------
 
 architecture str of controller is
+  component intermediate_reg
+    generic(N : integer:=7);
+    port(input : in std_logic_vector(N downto 0);
+         clk : in std_logic;
+         output : out std_logic_vector(N downto 0)
+         );
+    end component;
 
-
+signal    enable_write_sig  :  std_logic_vector(2 downto 0);
+signal    mux_in_sig        :  std_logic_vector(2 downto 0);
+signal    add_sub_sig       :  std_logic_vector(2 downto 0);
+signal    enable_output_sig :  std_logic_vector(2 downto 0);
+signal    load_sig          :  std_logic_vector(2 downto 0);
+signal    offset_sig        :  std_logic_vector(2 downto 0);
+signal    compare_sig       :  std_logic_vector(2 downto 0);
+signal    nop_sig           :  std_logic_vector(2 downto 0);
+  
 begin  -- architecture str
 
-  enable_write  <= I(7) nand I(6);
-  mux_in        <= I(7);
-  add_sub       <= (not I(6)) or I(7);
-  enable_output <= I(7) and I(6) and I(5);
-  load          <= (not I(6)) and I(7);
-  offset        <= I(0);
-  compare       <= I(7) and I(6) and not I(5);
+  enable_write_sig(2)  <= I(7) nand I(6);
+  mux_in_sig(2)        <= I(7);
+  add_sub_sig(2)       <= (not I(6)) or I(7);
+  enable_output_sig(2) <= I(7) and I(6) and I(5) and not I(0);
+  load_sig(2)          <= (not I(6)) and I(7);
+  offset_sig(2)        <= I(0);
+  compare_sig(2)       <= I(7) and I(6) and not I(5);
+  nop_sig(2)           <= I(7) and I(6) and I(5) and I(0);
 
+  INT_REGS :
+  for i in 1 downto 0 generate
+    INT_REGX : intermediate_reg port map(
+      input(0) => enable_write_sig(i+1),
+      input(1) => mux_in_sig(i+1),
+      input(2) => add_sub_sig(i+1),
+      input(3) => enable_output_sig(i+1),
+      input(4) => load_sig(i+1),
+      input(5) => offset_sig(i+1),
+      input(6) => compare_sig(i+1),
+      input(7) => nop_sig(i+1),
+      clk => clk,
+      output(0) => enable_write_sig(i),
+      output(1) => mux_in_sig(i),
+      output(2) => add_sub_sig(i),
+      output(3) => enable_output_sig(i),
+      output(4) => load_sig(i),
+      output(5) => offset_sig(i),
+      output(6) => compare_sig(i),
+      output(7) => nop_sig(i)
+      );
+  end generate INT_REGS;
+
+  enable_write <= enable_write_sig;
+  mux_in <= mux_in_sig;
+  add_sub <= add_sub_sig;
+  enable_output <= enable_output_sig;
+  load <= load_sig;
+  offset <= offset_sig;
+  compare <= compare_sig;
+  nop <= nop_sig;
+
+  
 end architecture str;
 
 -------------------------------------------------------------------------------
--- controller_tb.vhdl
+++ controller_tb.vhdl
@@ -9,39 +9,46 @@ architecture behavioral of controller_tb is
 -- Declaration of the component to be instantiated
   component controller
     port(I             : in  std_logic_vector(7 downto 0);
-         enable_write  : out std_logic;
-         mux_in        : out std_logic;
-         add_sub       : out std_logic;
-         enable_output : out std_logic;
-         load          : out std_logic;
-         offset        : out std_logic;
-         compare       : out std_logic
+         clk           : in  std_logic;
+         enable_write  : out std_logic_vector(2 downto 0);
+         mux_in        : out std_logic_vector(2 downto 0);
+         add_sub       : out std_logic_vector(2 downto 0);
+         enable_output : out std_logic_vector(2 downto 0);
+         load          : out std_logic_vector(2 downto 0);
+         offset        : out std_logic_vector(2 downto 0);
+         compare       : out std_logic_vector(2 downto 0);
+         nop           : out std_logic_vector(2 downto 0)
          );
   end component;
 -- Specifies which entity is bound with the component
   signal I_sig : std_logic_vector(7 downto 0);
+  signal clk : std_logic;
   signal enable_write_sig,
     mux_in_sig,
     add_sub_sig,
     enable_output_sig,
     load_sig,
     offset_sig,
-    compare  : std_logic;
+    compare_sig,
+    nop_sig : std_logic_vector(2 downto 0);
 begin
 -- Component instantiation
   controller0 : controller port map(I             => I_sig,
+                                    clk           => clk,
                                     enable_write  => enable_write_sig,
                                     mux_in        => mux_in_sig,
                                     add_sub       => add_sub_sig,
                                     enable_output => enable_output_sig,
                                     load          => load_sig,
                                     offset        => offset_sig,
-                                    compare       => compare);
+                                    compare       => compare_sig,
+                                    nop           => nop_sig);
 -- This process does the real job
   process
     type pattern_type is record
 -- The inputs of the controller
       I_sig : std_logic_vector(7 downto 0);
+      clk : std_logic;
 -- Expected outputs of controller
       enable_write_sig,
         mux_in_sig,
@@ -49,17 +56,24 @@ begin
         enable_output_sig,
         load_sig,
         offset_sig,
-        compare  : std_logic;
+        compare_sig,
+        nop_sig : std_logic_vector(2 downto 0);
     end record;
 -- The patterns to apply
     type pattern_array is array (natural range <>) of pattern_type;
     constant patterns : pattern_array :=
-      (("00000000", '1', '0', '1', '0', '0', '0', '0'),
-       ("01000101", '1', '0', '0', '0', '0', '1', '0'),
-       ("00000101", '1', '0', '1', '0', '0', '1', '0'),
-       ("10110111", '1', '1', '1', '0', '1', '1', '0'),
-       ("11001101", '0', '1', '1', '0', '0', '1', '1'),
-       ("11111000", '0', '1', '1', '1', '0', '0', '0')
+      (("00000000", '0', "1UU", "0UU", "1UU", "0UU", "0UU", "0UU", "0UU", "0UU"),
+       ("00000000", '1', "11U", "00U", "11U", "00U", "00U", "00U", "00U", "00U"),
+       ("01000101", '0', "11U", "00U", "01U", "00U", "00U", "10U", "00U", "00U"),
+       ("01000101", '1', "111", "000", "001", "000", "000", "110", "000", "000"),
+       ("00000101", '0', "111", "000", "101", "000", "000", "110", "000", "000"),
+       ("00000101", '1', "111", "000", "110", "000", "000", "111", "000", "000"),
+       ("10110111", '0', "111", "100", "110", "000", "100", "111", "000", "000"),
+       ("10110111", '1', "111", "110", "111", "000", "110", "111", "000", "000"),
+       ("11001101", '0', "011", "110", "111", "000", "010", "111", "100", "000"),
+       ("11001101", '1', "001", "111", "111", "000", "001", "111", "110", "000"),
+       ("11111000", '0', "001", "111", "111", "100", "001", "011", "010", "000"),
+       ("11111000", '1', "000", "111", "111", "110", "000", "001", "001", "000")
        );
 
   begin
@@ -67,6 +81,7 @@ begin
     for n in patterns'range loop
 -- Set the inputs      
       I_sig <= patterns(n).I_sig;
+      clk <= patterns(n).clk;
 -- Wait for the result      
       wait for 1 ns;
 -- Check the output      
@@ -81,7 +96,11 @@ begin
       assert load_sig = patterns(n).load_sig
         report "bad output value load" severity error;
       assert offset_sig = patterns(n).offset_sig
-        report "bad output value" severity error;
+        report "bad output value offset" severity error;
+      assert compare_sig = patterns(n).compare_sig
+        report "bad output value compare" severity error;
+      assert nop_sig = patterns(n).nop_sig
+        report "bad output value nop" severity error;
     end loop;
     assert false report "end of test" severity note;
 -- Wait forever; this will finish the simulation
