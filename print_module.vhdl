1b03a0eade6c0413ceae404166dd35a3febf1a78
Collin Clark <crclark96@gmail.com>
2018-05-16 22:35:15 -0400

    Revert "add intermediate reg to print module"
    This reverts commit f6d1839f949f1f104e217ec610bf4909d499154e.

--- print_module.vhdl
+++ print_module.vhdl
@@ -6,7 +6,7 @@
 -- Author     : Collin Clark  <collinclark@wifi-roaming-128-4-159-135.host.udel.edu>
 -- Company    : 
 -- Created    : 2018-04-09
--- Last update: 2018-05-15
+-- Last update: 2018-05-11
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -16,7 +16,7 @@
 -------------------------------------------------------------------------------
 -- Revisions  :
 -- Date        Version  Author  Description
--- 2018-04-09  1.0      collinclark     Created
+-- 2018-04-09  1.0      collinclark	Created
 -------------------------------------------------------------------------------
 
 library ieee;
@@ -28,10 +28,10 @@ use ieee.numeric_std.all;
 entity print_module is
 
   port (
-    input  : in  std_logic_vector(7 downto 0);
-    enable : in  std_logic;
-    nop    : in  std_logic;
-    clk    : in  std_logic;
+    input : in std_logic_vector(7 downto 0);
+    enable : in std_logic;
+    nop : in std_logic;
+    clk : in std_logic;
     output : out std_logic_vector(7 downto 0)
     );
 
@@ -41,35 +41,19 @@ end entity print_module;
 
 architecture str of print_module is
 
-  component intermediate_reg
-    generic(N : integer);
-    port(input  : in  std_logic_vector(N downto 0);
-         clk    : in  std_logic;
-         output : out std_logic_vector(N downto 0)
-         );
-  end component;
-
   -----------------------------------------------------------------------------
   -- Internal signal declarations
   -----------------------------------------------------------------------------
-  signal num     : integer := 0;
-  signal nop_sig : std_logic;
+  signal num : integer := 0;
 begin  -- architecture str
 
-  inter_reg0 : intermediate_reg generic map(N => 1)
-    port map(input(1)  => nop,
-             input(0) => '0',
-             clk    => clk,
-             output(1) => nop_sig
-             );
-
   num <= to_integer(signed(input));
-
+  
   process (clk)
   begin
     if rising_edge(clk) then
       if (enable = '1') then
-        if (nop_sig = '1') then
+        if (nop = '1') then
           report "nop";
           output <= "00000000";
         else
@@ -82,8 +66,8 @@ begin  -- architecture str
     end if;
   end process;
 
-
-
+  
+  
   -----------------------------------------------------------------------------
   -- Component instantiations
   -----------------------------------------------------------------------------
